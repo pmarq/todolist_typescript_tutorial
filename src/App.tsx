ce97e2ae3fa9403333dbb6cdbdda66fd0a2ad233
DESKTOP-VUTV05S\usuario <paulomuniz2411@gmail.com>
2023-02-11 16:37:27 -0300

    Feature enter add to list created

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import{ ListItem } from './components/ListItem';
 import { AddArea } from './components/AddArea';
 
 
-/*tuotrial -Bonieky Lacerda -  https://www.youtube.com/watch?v=95sAtAareR8 */
+/*tutrial -Bonieky Lacerda -  https://www.youtube.com/watch?v=95sAtAareR8 */
 
 function App() {
 
@@ -15,12 +15,22 @@ function App() {
     {id: 3 , name: 'Comprar leite na padaria', done:false}
   ]);
 
+  const handleAddTask = (taskName: string) =>{
+    let newList = [...list];
+    newList.push({
+      id:list.length +1,
+      name:taskName,
+      done:false
+    });
+    setList(newList);
+  }
+
   return (
     <C.Container>
       <C.Area>
       <C.Header>Lista de Tarefas</C.Header>
 
-       <AddArea />
+       <AddArea onEnter={handleAddTask} />
 
       {list.map((item, index)=>(
         <ListItem key={index} item={item}/>
