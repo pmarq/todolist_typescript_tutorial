47d0dc6e12fd33c6be5aadba89d61859a68cfe97
DESKTOP-VUTV05S\usuario <paulomuniz2411@gmail.com>
2023-02-11 13:54:01 -0300

    new folder types and item file

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
 import { useState } from 'react';
 import * as C from './App.styles';
+import { Item } from './types/item';
 
 
 function App() {
 
-  const [list, setList] = useState([]);
+  const [list, setList] = useState<Item[]>([
+    { id: 1, name: 'Comprar pão na padaria', done: false },
+    { id: 2, name: 'Comprar bolo na padaria', done: false },
+  ]);
 
   return (
     <C.Container>
+      <C.Area>
       <C.Header>Lista de Tarefas</C.Header>
+
+      {list.map((item, index)=>(
+        <div>{item.name}</div>
+      ))}
+
+      </C.Area>
     </C.Container>
  
   );
--- src/types/item.ts
+++ src/types/item.ts
@@ -1,4 +1,4 @@
-export type Iten = {
+export type Item = {
     id: number;
     name: string;
     done: boolean;
